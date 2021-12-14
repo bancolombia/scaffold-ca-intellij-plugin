d81f14af0a12e44153e25b602b3923b81aac1861
Juan Carlos <jakspok@gmail.com>
2021-12-14 09:33:56 -0500

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,14 +40,14 @@ changelog {
     groups.set(emptyList())
 }
 
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
+/*// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
 qodana {
     cachePath.set(projectDir.resolve(".qodana").canonicalPath)
     reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
     saveReport.set(true)
     showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
     autoUpdate.set(true)
-}
+}*/
 
 tasks {
     // Set the JVM compatibility versions
