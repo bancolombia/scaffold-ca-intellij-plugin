ada740e7d20ae54995fbaa7f3cdbd3129b7e3eed
Juan Carlos <jakspok@gmail.com>
2022-10-06 10:11:24 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -171,7 +171,3 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "11"
 }
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
-}
